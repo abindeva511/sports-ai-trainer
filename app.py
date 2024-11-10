7f58ea9c0759e2f6fda295c669d392bd30e66140
abindeva511 <abdinfire1@gmail.com>
2024-11-10 14:10:17 -0800

    dd

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from sentence_transformers import SentenceTransformer
 from testing import save_and_render_frames
 
 # Load the embedding model
-embedding_model = SentenceTransformer('all-mpnet-base-v2', device='cpu', trust_remote_code=True)
+embedding_model = SentenceTransformer('paraphrase-MiniLM-L6-v2', device='cpu', trust_remote_code=True)
 
 # Set upload folder
 UPLOAD_FOLDER = 'uploads'
